60e6506b0ed5fd7dcab21d7a7c0f730035f4c4d5
Lara Costa <larasmila64@gmail.com>
2024-01-03 12:48:39 -0300

    feat: importing bucket from aws console created manually to be managed by terraform

--- terraform_basico/interpolacao/s3.tf
+++ terraform_basico/interpolacao/s3.tf
@@ -10,3 +10,12 @@ resource "aws_s3_object" "my-bucket-test" {
   source = local.ip_filepath
   etag   = filemd5(local.ip_filepath)
 }
+
+resource "aws_s3_bucket" "manual" {
+  bucket = "mybucketawslaracosta"
+  tags = {
+    Created   = "03-01-2024"
+    Imported  = "03-01-2024"
+    ManagedBy = "Terraform"
+  }
+}
